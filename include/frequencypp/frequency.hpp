b5c4d60061b204e8bbc09c55691113d440e97d94
Jeremiah Griffin <nokurn@gmail.com>
2021-10-31 13:33:16 -0700

    Add declaration of frequency type

--- include/frequencypp/frequency.hpp
+++ include/frequencypp/frequency.hpp
@@ -12,7 +12,51 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+/// \file
+/// Contains the temporal frequency type \ref frequencypp::frequency and its associated types and
+/// specializations
+
 #ifndef FREQUENCYPP_FREQUENCY_HPP
 #define FREQUENCYPP_FREQUENCY_HPP
+
+#include <ratio>
+
+// Forward declaration of the frequencypp::frequency type
+
+namespace frequencypp {
+
+template<typename Rep, typename Period = std::ratio<1>>
+struct frequency;
+
+} // namespace frequencypp
+
+// Types needed to implement frequency, which may refer to the above forward declaration
+
+namespace frequencypp {
+
+/// Represents a temporal frequency
+///
+/// A frequency consists of a count of ticks of type \p Rep and a tick period \p Period, where the
+/// tick period is a compile-time rational fraction representing the frequency in hertz from one
+/// tick to the next.
+///
+/// The only data stored in a \ref frequencypp::frequency is a tick count of type \p Rep.  If \p Rep
+/// is floating point, then the \ref frequencypp::frequency can represent fractions of ticks.
+/// \p Period is included as part of the type and is only used when converting between different
+/// frequencies.
+///
+/// \tparam Rep arithmetic type representing the number of ticks
+/// \tparam Period ratio representing the tick period
+template<typename Rep, typename Period>
+struct frequency
+{
+public:
+    /// Arithmetic type representing the number of ticks
+    using rep = Rep;
+    /// Ratio representing the tick period (i.e. the number of hertz fractions per tick)
+    using period = typename Period::type;
+};
+
+} // namespace frequencypp
 
 #endif // FREQUENCYPP_FREQUENCY_HPP
